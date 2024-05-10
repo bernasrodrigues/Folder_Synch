646b1090ca324d9450f503f9c8b63be7ccb417be
bernasrodrigues <47544028+bernasrodrigues@users.noreply.github.com>
2024-05-10 04:03:46 +0100

    refactoring and added test cases

--- main.py
+++ main.py
@@ -27,32 +27,44 @@ class SyncManager:
         self.replica = replica
         self.logger = logger
 
-        self.logger.log_activity(f"Staring Synchronization")
+        self.logger.log_activity(f"Starting Synchronization")
 
     def synchronize(self):
         """Synchronizes files and folders between the source and replica directories."""
         self.logger.log_activity("Sync operation started")
 
-        source_files, source_dirs = self.scan_directory(self.source)  # get the files and directories in source folder
-        replica_files, replica_dirs = self.scan_directory(self.replica) # get the files and directories in replica folder
+        try:
+            source_files, source_dirs = self.scan_directory(self.source)
+            replica_files, replica_dirs = self.scan_directory(self.replica)
 
-        # delete excess folders in replica
+            self.delete_excess_folders(replica_dirs, source_dirs)
+            self.create_missing_folders(replica_dirs, source_dirs)
+            self.update_or_create_files(source_files, replica_files)
+            self.delete_excess_files(replica_files, source_files)
+
+            self.logger.log_activity("Sync operation completed")
+
+        except Exception as e:
+            self.logger.log_activity(f"Error occurred during synchronization: {e}")
+
+    def delete_excess_folders(self, replica_dirs, source_dirs):
+        """Delete excess folders in the replica directory."""
         for directory in replica_dirs:
             if directory not in source_dirs:
                 subfolder_to_delete = os.path.join(self.replica, directory)
-                if os.path.isdir(subfolder_to_delete):
-                    shutil.rmtree(subfolder_to_delete)
-                    self.logger.log_activity(f"Deleted folder: {subfolder_to_delete}")
+                shutil.rmtree(subfolder_to_delete)
+                self.logger.log_activity(f"Deleted folder: {subfolder_to_delete}")
 
-        # Create missing folders in replica directory
+    def create_missing_folders(self, replica_dirs, source_dirs):
+        """Create missing folders in the replica directory."""
         for directory in source_dirs:
             if directory not in replica_dirs:
                 subfolder_to_create = os.path.join(self.replica, directory)
-                if not os.path.isdir(subfolder_to_create):
-                    os.makedirs(subfolder_to_create)
-                    self.logger.log_activity(f"Created folder: {subfolder_to_create}")
+                os.makedirs(subfolder_to_create)
+                self.logger.log_activity(f"Created folder: {subfolder_to_create}")
 
-        # Update or create files
+    def update_or_create_files(self, source_files, replica_files):
+        """Update or create files in the replica directory."""
         for file in source_files:
             source_file_path = os.path.join(self.source, file)
             replica_file_path = os.path.join(self.replica, file)
@@ -65,54 +77,13 @@ class SyncManager:
                 shutil.copy(source_file_path, replica_file_path)
                 self.logger.log_activity(f"Created file: {replica_file_path}")
 
-        # Remove excess files
+    def delete_excess_files(self, replica_files, source_files):
+        """Delete excess files in the replica directory."""
         for file in replica_files:
             if file not in source_files:
                 replica_file_path = os.path.join(self.replica, file)
                 os.remove(replica_file_path)
                 self.logger.log_activity(f"Deleted file: {replica_file_path}")
-
-        self.logger.log_activity("Sync operation completed")
-
-
-
-        '''
-        files_to_create = [file for file in source_files if file not in replica_files]  # files missing in replica folder
-        files_to_delete = [file for file in replica_files if file not in source_files]  # excess files in source folder
-        files_to_update = [file for file in source_files if file in replica_files]  # files with the same names in both folders that we will compare the content
-
-        # Check if the content of the files is the same in both files (by hashing their contents)
-        if files_to_update:
-            for file in files_to_update:
-                source_file = os.path.join(self.source, file)
-                replica_file = os.path.join(self.replica, file)
-                if not self.compare_hashes(source_file, replica_file):  # comparing the files
-                    replica_file_path = os.path.join(self.replica, file)
-                    os.remove(replica_file_path)
-
-                    source_file_path = os.path.join(self.source, file)
-                    replica_file_path = os.path.join(self.replica, file)
-                    shutil.copy(source_file_path, replica_file_path)
-
-                    self.logger.log_activity(f"File '{replica_file_path} was modified")
-                else:
-                    self.logger.log_activity(f"File '{file}' is up to date.")  # <- TODO may fill log file with redundant info (maybe remove so that only changes are noted)
-
-        # Remove excess files
-        for file in files_to_delete:
-            replica_file_path = os.path.join(self.replica, file)
-            os.remove(replica_file_path)
-            self.logger.log_activity(f"Deleted file: {replica_file_path}")
-
-        # Create missing files
-        for file in files_to_create:
-            source_file_path = os.path.join(self.source, file)
-            replica_file_path = os.path.join(self.replica, file)
-            shutil.copy(source_file_path, replica_file_path)
-            self.logger.log_activity(f"Created file: {replica_file_path}")
-
-        self.logger.log_activity("Sync operation completed")
-        '''
 
     @staticmethod
     def scan_directory(directory_path: str):
@@ -131,31 +102,28 @@ class SyncManager:
     @staticmethod
     def compare_hashes(source_file, replica_file):
         """Hashes chunks of 2 files and compares them to check if they are identical."""
-        # print(f"Comparing files '{source_file}' with '{replica_file}'")
-
         block_size = 65536  # 64 KB chunks
+
         with open(source_file, 'rb') as source, open(replica_file, 'rb') as replica:
-            source_hash = hashlib.md5()         # md5 -> fastest hash that provides decent results
+            source_hash = hashlib.md5()
             replica_hash = hashlib.md5()
 
             while True:
                 source_chunk = source.read(block_size)
                 replica_chunk = replica.read(block_size)
 
-                # If both source_chunk and replica_chunk are empty, it means we reached the end of both files
                 if not source_chunk and not replica_chunk:
                     break
 
-                # If one chunk is empty while the other is not, files have different sizes and are considered different
-                if (source_chunk and not source_chunk) or (not source_chunk and replica_chunk):
-                    # print("Files are different")
+                if (source_chunk and not replica_chunk) or (not source_chunk and replica_chunk):
                     return False
 
                 source_hash.update(source_chunk)
                 replica_hash.update(replica_chunk)
-                if source_hash.digest() != replica_hash.digest():    # if a chunk is different return false (meaning files' contents are diferent)
-                    # print("Files are different")
+
+                if source_hash.digest() != replica_hash.digest():
                     return False
+
         return True
 
 
@@ -165,23 +133,29 @@ class SyncScheduler:
         :param source: source folder
         :param replica: replica folder
         :param log_path: name of the logger file
-        :param sync_interval: after each synchronization, wait "sync_interval" seconds until next synch
+        :param sync_interval: after each synchronization, wait "sync_interval" seconds until next synchronization
         """
         self.source = source
         self.replica = replica
         self.log_path = log_path
         self.sync_interval = sync_interval
         self.logger = Logger(log_path)
+
+        # Check if source and replica folders exist
+        if not os.path.exists(source):
+            print(f"Error: Source folder '{source}' does not exist.")
+            sys.exit(1)
+        if not os.path.exists(replica):
+            print(f"Warning: Replica folder '{replica}' does not exist. Folder will be created")
+
         self.sync_manager = SyncManager(source, replica, self.logger)
 
     def run(self):
         try:
             while True:
-                print(f"--------------------------------\n"
-                      f"Sync operation in progress.\n")
+                print("--------------------------------\nSync operation in progress.\n")
                 self.sync_manager.synchronize()
-                print(f"\n"
-                      f"Sync operation completed. (Syncing again in {self.sync_interval} seconds)")
+                print(f"\nSync operation completed. (Syncing again in {self.sync_interval} seconds)")
                 time.sleep(self.sync_interval)
 
         except KeyboardInterrupt:
@@ -191,6 +165,7 @@ class SyncScheduler:
 
 class SyncThread(threading.Thread):
     """Runs the SyncScheduler in a thread"""
+
     def __init__(self, scheduler):
         super().__init__()
         self.scheduler = scheduler
---
+++ test_cases.py
@@ -0,0 +1,47 @@
+import os
+import unittest
+
+from main import SyncManager, Logger
+
+
+class TestSyncManager(unittest.TestCase):
+    def setUp(self):
+        # Set up source and replica folder paths relative to the current directory
+        self.current_dir = os.getcwd()
+        self.source_dir = os.path.join(self.current_dir, "test_source_folder")
+        self.replica_dir = os.path.join(self.current_dir, "test_replica_folder")
+
+        # Initialize logger and SyncManager
+        self.logger = Logger("test_log.txt")
+        self.logger.log_activity("Starting tests")
+        self.sync_manager = SyncManager(self.source_dir, self.replica_dir, self.logger)
+
+    def tearDown(self):
+        # Clean up source and replica folders
+        self.logger.log_activity("Completed tests")
+
+    def test_sync_files_identical(self):
+        """Test if synchronized files are identical."""
+        # Perform synchronization
+        self.sync_manager.synchronize()
+
+        # Verify that files in the replica folder are identical to files in the source folder
+        file1_source_path = os.path.join(self.source_dir, "File_1.txt")
+        file1_replica_path = os.path.join(self.replica_dir, "File_1.txt")
+        with open(file1_source_path, "r") as source_file, open(file1_replica_path, "r") as replica_file:
+            source_content = source_file.read()
+            replica_content = replica_file.read()
+            self.assertEqual(source_content, replica_content)
+
+        file2_source_path = os.path.join(self.source_dir, "test_folder", "File_2.txt")
+        file2_replica_path = os.path.join(self.replica_dir, "test_folder", "File_2.txt")
+        with open(file2_source_path, "r") as source_file, open(file2_replica_path, "r") as replica_file:
+            source_content = source_file.read()
+            replica_content = replica_file.read()
+            self.assertEqual(source_content, replica_content)
+
+        self.logger.log_activity("Test for synchronizing files passed")
+
+
+if __name__ == '__main__':
+    unittest.main()
